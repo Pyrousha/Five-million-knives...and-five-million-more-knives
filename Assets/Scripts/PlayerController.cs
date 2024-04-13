5c637a351a4a40542298ed3d525414455ae9ffc9
pascal <pmkgbwis@gmail.com>
2024-04-12 22:31:36 -0700

    very basic player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rbody;
+    [SerializeField] private GroundedCheck ground;
+
+    private bool grounded;
     void FixedUpdate()
     {
+        grounded = ground.CheckGrounded();
+
+        HandleInputs();
+    }
+
+    void HandleInputs() {
+        var velocity = rbody.velocity;
         
+        if (InputHandler.Instance.move.down) {
+            velocity.x = InputHandler.Instance.dir * speed;
+        } else {
+            velocity.x = 0;
+        }
+
+        if (grounded && InputHandler.Instance.jump.pressed) {
+            velocity.y = 10;
+        }
+
+        rbody.velocity = velocity;
     }
 }
